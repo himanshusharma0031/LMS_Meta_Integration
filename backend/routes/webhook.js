e8169c93a102b8b8c6d3255ee10f25d5c5049b44
Himanshu Sharma <himanshusharma22902@gmail.com>
2026-06-20 21:52:27 -0700

    meta changes

--- backend/routes/webhook.js
+++ backend/routes/webhook.js
@@ -128,7 +128,7 @@ module.exports = router
 //   })
 // }
 
-// // Website leads
+// // Website lead
 // router.post('/website', async (req, res) => {
 //   try {
 //     const lead = await Lead.create({
